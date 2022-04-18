aab35fa0f833e5d1f5096a5991e530b9e2806469
LeeJuOh <xop4p@naver.com>
2022-04-18 23:17:02 +0900

    update order pizza

---
+++ src/factory/before/Main.java
@@ -0,0 +1,25 @@
+package factory.before;
+
+public class Main {
+
+    Pizza orderPizza(String type) {
+        Pizza pizza = null;
+
+        if (type.equals("cheese")) {
+            pizza = new CheesePizza();
+        } else if (type.equals("greek")) {
+            pizza = new GreekPizza();
+        } else if (type.equals("pepperoni")) {
+            pizza = new PepperoniPizza();
+        }
+
+        pizza.prepare();
+        pizza.bake();
+        pizza.cut();
+        pizza.box();
+
+        return pizza;
+    }
+
+
+}
